4ebb48a8c82747bf640df694e36df69eb75a91c7
suryaofficial7 <suryaskofficial7@gmail.com>
2024-03-15 08:42:42 +0530

    practical 3b FRAGMENTS and MULTIPLE FRAGMENTS Done

---
+++ PRACT 4/app/src/main/java/com/example/pract1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pract1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "ON - CREATE", Toast.LENGTH_SHORT).show()
+    }
+
+}
